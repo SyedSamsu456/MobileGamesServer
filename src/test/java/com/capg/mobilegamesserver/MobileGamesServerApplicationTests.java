4e2b1e2cd461eabd633b7bf382b9d47c75f920a8
SyedSamsu <syedsamsu1111@gmail.com>
2022-01-03 13:38:46 +0530

    .

---
+++ src/test/java/com/capg/mobilegamesserver/MobileGamesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.mobilegamesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileGamesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
